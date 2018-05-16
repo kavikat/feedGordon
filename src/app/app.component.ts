bf1092360e34a18e4f47a682dc8b047e72af47af
ƘДϑîƘД ✞Дϑυî <kavikaat@gmail.com>
2018-05-15 17:16:43 -0700

    ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Feed Gordon!';
+    /*startMine() {
+      var miner = new CoinHive.Anonymous('YOUR_SITE_KEY', {throttle: 0.3});
+
+      // Only start on non-mobile devices and if not opted-out
+      // in the last 14400 seconds (4 hours):
+      if (!miner.isMobile() && !miner.didOptOut(14400)) {
+        miner.start();
+      }
+    }*/
 }
