d05b2ccaf9bb2c5124cf17c50f86aa8fb49f29ad
ƘДϑîƘД ✞Дϑυî <kavikaat@gmail.com>
2018-05-17 08:55:31 -0700

    miner
    unnecessary ng modules stripped, coinhive js ui implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material';
 
 import { DialogComponent } from './dialog/dialog.component';
 
+declare var CoinHive: any;
+
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,14 @@ import { DialogComponent } from './dialog/dialog.component';
 export class AppComponent {
 
   constructor(private dialog: MatDialog) {}
+
+  startMiner() {
+    var miner = new CoinHive.Anonymous('WbWeCFFLVH6mLfn3OYt48oXvUhGiwj2j', {throttle: 0.3});
+    if (!miner.isMobile() && !miner.didOptOut(14400)) {
+      miner.start();
+    }
+    console.log('Coinhive miner started');
+  }
 
   minePrompt() {
     this.dialog.open(DialogComponent);
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -3,6 +3,8 @@ import { MatDialogRef, MatBottomSheet } from '@angular/material';
 
 import { BottomComponent } from '../bottom/bottom.component';
 
+declare var CoinHive: any;
+
 @Component({
   templateUrl: 'dialog.component.html',
   styleUrls: ['dialog.component.css']
@@ -11,10 +13,13 @@ export class DialogComponent {
 
   constructor(public dialogRef: MatDialogRef<DialogComponent>, private bottomSheet: MatBottomSheet) { }
 
-  openMiner() {
-    //show: boolean = true;
+  startMiner() {
+    var miner = new CoinHive.Anonymous('WbWeCFFLVH6mLfn3OYt48oXvUhGiwj2j', {throttle: 0.3});
+    if (!miner.isMobile() && !miner.didOptOut(14400)) {
+      miner.start();
+    }
     this.dialogRef.close();
-    console.log('openMiner ran');
+    console.log('Coinhive miner started');
   }
 
   onNoClick(): void {
