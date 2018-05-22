ff6b619c97ad09feac538a09e9fd433f96375dbd
ƘДϑîƘД ✞Дϑυî <kavikaat@gmail.com>
2018-05-21 20:04:18 -0700

    UI

---
+++ src/app/prompt.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+
+/**
+ * @title Snack-bar with a custom component
+ */
+@Component({
+  selector: 'prompt-component',
+  templateUrl: '.html',
+})
+export class SnackBarComponentExample {
+  constructor(public snackBar: MatSnackBar) {}
+
+  openSnackBar() {
+    this.snackBar.openFromComponent(PizzaPartyComponent, {
+      duration: 500,
+    });
+  }
+}
